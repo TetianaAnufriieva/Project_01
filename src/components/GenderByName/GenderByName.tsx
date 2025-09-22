764ea41767fe785701c68270e5d0c67129b79403
TetianaAnufriieva <anufrieva.tetiana@gmail.com>
2025-09-22 22:33:58 +0200

    update hw_04

--- src/components/GenderByName/GenderByName.tsx
+++ src/components/GenderByName/GenderByName.tsx
@@ -1,30 +1,40 @@
-import { useEffect, useState } from "react";
-import styles from "./GenderByName.module.css"
+import { useState } from "react";
+import styles from "./GenderByName.module.css";
 const URL_GenderByName = "https://api.genderapi.io/api/?name=";
 
+interface GenderResponse {
+  name: string;
+  gender: string | null;
+  country: string;
+  probability: number;
+  remaining_credits: number;
+}
+
 export default function GenderByName() {
-  const [name, setName] = useState("Tetiana");
-  const [data, setData] = useState<any>();
+  const [name, setName] = useState<string>("");
+  const [data, setData] = useState<GenderResponse | null>(null);
   const [message, setMessage] = useState("");
 
   async function getGenderByName() {
-
     if (!name.trim()) {
       setMessage("Enter the name");
       setData(null);
       return;
     }
 
-
     try {
       const res = await fetch(URL_GenderByName + name);
       if (!res.ok) {
-        if (res.status === 404) {
+        if (res.status === 429) {
           throw new Error("Too many requests");
+        } else {
+          throw new Error(`Ошибка: ${res.status}`);
         }
       }
-      const obj = await res.json();
+
+      const obj: GenderResponse = await res.json();
       setData(obj);
+      setMessage("");
       console.log(obj);
     } catch (error) {
       if (error instanceof Error) {
@@ -32,11 +42,6 @@ export default function GenderByName() {
       }
     }
   }
-
-  useEffect(() => {
-    getGenderByName();
-  }, [name]);
-
   return (
     <div className={styles.container}>
       <h2>Gender By Name</h2>
@@ -51,23 +56,25 @@ export default function GenderByName() {
       {message ? (
         <p>{message}</p>
       ) : (
-        <div>
-          <p>
-            <b>Имя:</b> {data?.name}
-          </p>
-          <p>
-            <b>Пол:</b> {data?.gender === "male" ? "мужской" : "женский"}
-          </p>
-          <p>
-            <b>Страна:</b> {data?.country}
-          </p>
-          <p>
-            <b>Вероятность:</b> {data?.probability}%
-          </p>
-          <p>
-            <b>Остаток запросов:</b> {data?.remaining_credits}
-          </p>
-        </div>
+        data && (
+          <div>
+            <p>
+              <b>Имя:</b> {data.name}
+            </p>
+            <p>
+              <b>Пол:</b> {data.gender === "male" ? "мужской" : "женский"}
+            </p>
+            <p>
+              <b>Страна:</b> {data.country}
+            </p>
+            <p>
+              <b>Вероятность:</b> {data.probability}%
+            </p>
+            <p>
+              <b>Остаток запросов:</b> {data.remaining_credits}
+            </p>
+          </div>
+        )
       )}
     </div>
   );
